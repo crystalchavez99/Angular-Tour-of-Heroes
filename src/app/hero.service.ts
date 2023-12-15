207a21686706268740d2ac85c08478c65a0b4b2e
Crystal <crystal120599@gmail.com>
2023-12-15 10:28:53 -0800

    applied messaging service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 // This marks the class as one that participates in the dependency injection system.
 //  accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
@@ -10,9 +11,10 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,7 @@ import {FormsModule} from '@angular/forms';
 import {Hero} from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   standalone: true,
   selector: 'app-heroes',
@@ -28,15 +29,18 @@ export class HeroesComponent {
   selectedHero?: Hero;
   // dentifies it as a HeroService injection site.
   // heroService parameter to the singleton instance of HeroService.
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService,  private messageService: MessageService) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  ngOnInit(): void {
-    this.getHeroes();
   }
 }
